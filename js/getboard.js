064d7b61ce92baef3ae0a8be5fdf594c09c8e67b
Mikhail Rakhunov <mrakhunov@gmail.com>
2016-12-11 09:28:29 -0600

    12/11/2016 9:28 AM

--- js/getboard.js
+++ js/getboard.js
@@ -14,7 +14,7 @@
 	isNotationShown: false,
 	isChess: true,
 	isShowSettings: true,
-	version: function() { $('#curr-version').html("&copy2014 Mikhail Rakhunov. Checkers Home(1.2.14)"); },
+	version: function() { $('#curr-version').html("&copy2014 Mikhail Rakhunov. Checkers Home(1.2.16)"); },
 	cellbackground:  {
 		cellblue:['#7DBDE7', 'rgba(125, 189, 231, 0)', 'rgb(35, 137, 231)'], /* black cell, white cell, and board border*/
 	    cellbrown:['#A45200', '#FBD584', '#990000'],
@@ -371,83 +371,6 @@ var boardColorChange = function(colorName) {
 	$('table').css('border-color',Board.cellbackground[colorName][2]);
 }
 
-var MRE = function (txt) {
-	var k = 3;
-	var r = Math.random()*3;
-	if (r < 1) k = 3;
-	else if (r > 2) k = 7;
-	else k = 5;
-	r = k;
-	var out = ""; 
-	for (i=0; i<txt.length; i++){
-	   num = txt.charCodeAt(i);
-	   tmp=String(num *k);
-	   if(tmp.length < 2) out = "000"+tmp;
-	   else if (tmp.length < 3) tmp = "00"+tmp;
-	   else if (tmp.length < 4) tmp = "0"+tmp;
-	   out +=tmp;
-	   if (k == 3) k = 5;
-	   else if (k == 5) k = 7; 
-	   else k = 3; 
-	}
-	return String(r) + out;
-}
-
-var MRD = function (inTxt) {
-
-	var left = inTxt.substr(1);
-	var r = inTxt.substr(0,1);
-	var pos = 4;
-	var txt = "";
-	var num = 0; 
-	k = Number(r);
-
-	while (left.length >=pos){
-	 num =  Number(left.slice(0, pos))/k;
-	 txt += String.fromCharCode(num);
-	 
-	 left = left.slice(pos);
-	 k += 2;
-	 if (k > 7) k = 3; 
-	}
-	return txt; 
-}
-
-var MREn = function (numArray) {
-    var newArray = [];
-	var k = 13;
-	var r = Math.random()*3;
-	if (r < 1) k = 13;
-	else if (r > 2) k = 17;
-	else k = 19;
-	r = k;
-	
-	for (var i = 0; i < numArray.length; i++){
-	  var nbr = parseInt(numArray[i]);
-	   newArray[i] = nbr + k;
-	   if (k == 13) k = 17;
-	   else if (k == 17) k = 19; 
-	   else k = 13; 
-	}
-	newArray[newArray.length] = r;
-	return newArray;
-}
-
-var MRDn = function (inNumArray) {
-	var newArray = [];
-	var r = inNumArray[inNumArray.length - 1];
-	var k = r;
-	for (var i = 0; i < inNumArray.length - 1; i++){
-	   newArray[i] = inNumArray[i] - k;
-	   if (k == 13) k = 17;
-	   else if (k == 17) k = 19; 
-	   else k = 13; 
-	}
-	
-	return newArray; 
-}
-
-//***
  var createBoard = function(elId) {
 	var $div = document.createElement("div");
 	Board2.parenttId = "board" + Board2.id;
